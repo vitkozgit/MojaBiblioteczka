b343a3a0d60e814feb591d27697be4cb4f252f19
vitkozgit <vitaligit@gmail.com>
2017-07-02 19:39:41 +0200

    Skeleton of loading the stencils

--- src/main/java/pl/moja/biblioteczka/controllers/TopMenuButtonsController.java
+++ src/main/java/pl/moja/biblioteczka/controllers/TopMenuButtonsController.java
@@ -1,27 +1,43 @@
 package pl.moja.biblioteczka.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ToggleGroup;
 
 public class TopMenuButtonsController {
 
+    private static final String LIST_BOOKS_FXML = "/fxml/ListBooks.fxml";
+    private static final String LIBRARY_FXML = "/fxml/Library.fxml";
+    private static final String STATISTICS_FXML = "/fxml/Statistics.fxml";
     private MainController mainController;
 
     @FXML
-    public void openLibrary() {
+    private ToggleGroup toogleButtons;
 
+    @FXML
+    public void openLibrary() {
+        mainController.setCenter(LIBRARY_FXML);
     }
 
     @FXML
     public void openListBooks() {
-
+        mainController.setCenter(LIST_BOOKS_FXML);
     }
 
     @FXML
     public void openStatistics() {
-
+        mainController.setCenter(STATISTICS_FXML);
     }
 
     public void setMainController(MainController mainController) {
         this.mainController = mainController;
+    }
+
+    @FXML
+    public void addBook() {
+        if(toogleButtons.getSelectedToggle() != null) {
+            toogleButtons.getSelectedToggle().setSelected(false);
+        }
+
+        mainController.setCenter("/fxml/addBook.fxml");
     }
 }
